951637afd0e2b9be4f787275cf540f8978b69ea6
Andrew Steinmeyer <jandrewsteinmeyer@gmail.com>
2014-06-03 14:46:11 -0400

    updating description of recipe

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email  "jandrewsteinmeyer@gmail.com"
 description       "Installs the php5-fpm"
 version           "0.0.1"
 
-recipe "php5-fpm", "php5-fpm server"
+recipe "php5-fpm", "Installs php5-fpm"
 
 supports "ubuntu"
